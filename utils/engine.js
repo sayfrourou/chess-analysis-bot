f2786fffff66f89d40223cc69867377a26b81291
sayf <sayfrourou@gmail.com>
2023-04-18 11:11:01 +0100

    update

---
+++ utils/engine.js
@@ -0,0 +1,78 @@
+const { VARS } = require('../VARS')
+const { executeEngine } = require('./process')
+
+class ChessEngine {
+  constructor() {
+    this.fen = []
+  }
+
+  isWhiteTurn() {
+    return this.turn === 'w'
+  }
+
+  getTurn() {
+    return this.turn
+  }
+
+  getLastFen() {
+    var pos = this.fen.length == 0 ? 0 : this.fen.length - 1
+
+    return this.fen[pos]
+  }
+
+
+
+  reset() {
+    this.turn = 'w'
+  }
+
+  async start(engine_mode, turn, depth, movetime, engine_name, fen) {
+    this.engine_mode = engine_mode
+    this.turn = turn
+    this.depth = depth
+    this.movetime = movetime
+    this.engine = engine_name
+    this.fen.push(fen)
+
+
+
+
+    if (this.engine_mode == VARS.DEPTH_MODE) {
+      console.log("using depth mode")
+      const engineResult = await executeEngine(
+        `position fen ${this.getLastFen()}\n`,
+        `go depth ${this.depth}`,
+        this.engine
+      )
+      return {
+        fen: this.getLastFen(),
+        turn: this.turn,
+        setDepth: this.depth,
+        engineDepth: engineResult.depth,
+        selDepth: engineResult.seldepth,
+        bestMove: engineResult.bestmove,
+        possibleHumanMove: engineResult.possible_human_move,
+      }
+    } else {
+      console.log("using movetime mode")
+
+      const engineResult = await executeEngine(
+        `position fen ${this.getLastFen()}\n`,
+        `go movetime ${this.movetime}`,
+        this.engine
+      )
+      return {
+        fen: this.getLastFen(),
+        turn: this.turn,
+        setDepth: this.depth,
+        engineDepth: engineResult.depth,
+        selDepth: engineResult.seldepth,
+        bestMove: engineResult.bestmove,
+        possibleHumanMove: engineResult.possible_human_move,
+      }
+    }
+
+  }
+}
+
+module.exports = { ChessEngine }
